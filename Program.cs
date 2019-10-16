83a6e9849424ed9a35dc3ee469a8f9e907c6867b
Priit Roosipuu <roosipuu.priit@gmail.com>
2019-10-16 14:55:21 +0300

    projekti algus

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metrics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
